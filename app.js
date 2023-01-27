363eb78947083bb16629f29e1212b6f2d80b5b4c
lucasburgosr <lucaasburgos13@gmail.com>
2023-01-27 20:54:37 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const colors = ["green", "red", "rgba(133, 122, 200)", "#f15025", "orange"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector("color");
+
+btn.addEventListener("click", function(){
+    //para conseguir un número al azar entre 0 y 3
+    const numAzar = numeroAleatorio();
+    document.body.style.backgroundColor = colors[numAzar];
+    color.textContent = colors[numAzar];
+});
+
+function numeroAleatorio(){
+    return Math.floor(Math.random()*colors.length);
+}
